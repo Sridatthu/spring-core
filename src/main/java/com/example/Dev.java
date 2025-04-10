23a0722da0947ae75a0184a60f1761786c5a3402
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-04-10 13:33:02 +0530

    2 objects creation

--- src/main/java/com/example/Dev.java
+++ src/main/java/com/example/Dev.java
@@ -1,6 +1,9 @@
 package com.example;
 
 public class Dev {
+    public Dev(){
+        System.out.println("Dev constructor");
+    }
     public void build(){
         System.out.println("built is running");
     }
---
+++ src/main/java/com/example/Laptop.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Laptop {
+    public  Laptop(){
+        System.out.println("laptop constructor");
+    }
+    public void compile(){
+        System.out.println("laptop is compiling");
+    }
+}
