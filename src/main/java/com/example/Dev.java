8b03e5e0873b8c0b3d70e10030d078bb1c7077b7
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-04-10 13:38:51 +0530

    setter injection for primitive

--- src/main/java/com/example/Dev.java
+++ src/main/java/com/example/Dev.java
@@ -4,7 +4,15 @@ public class Dev {
     public Dev(){
         System.out.println("Dev constructor");
     }
+    private int age;
     public void build(){
         System.out.println("built is running");
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
--- src/main/java/com/example/Laptop.java
+++ src/main/java/com/example/Laptop.java
@@ -4,7 +4,11 @@ public class Laptop {
     public  Laptop(){
         System.out.println("laptop constructor");
     }
+    
+    
     public void compile(){
         System.out.println("laptop is compiling");
     }
+
+    
 }
