4db78bab9a67e9078ccfc4f8cf4c7540836ad88e
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-04-10 14:24:59 +0530

    setter injection

--- src/main/java/com/example/Dev.java
+++ src/main/java/com/example/Dev.java
@@ -2,14 +2,18 @@ package com.example;
 
 public class Dev {
     private Computer comp;
-    public Dev(Computer comp){
-        this.comp=comp;
-    }
+    
     public Dev(){
         System.out.println("Dev constructor");
     }
     public void build(){
         comp.compile();
         System.out.println("built is running");
+    }
+    public Computer getComp() {
+        return comp;
+    }
+    public void setComp(Computer comp) {
+        this.comp = comp;
     }
 }
